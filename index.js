6ef6ca8b406c325787377f12947def1cf049f6ca
MDSELIM1 <selimakodo58@gmail.com>
2021-09-02 23:06:09 +0600

    add

--- index.js
+++ index.js
@@ -1,48 +1,41 @@
+// Error Handling Messege hide 
 document.getElementById('search-input').addEventListener('focus', function () {
   document.getElementById('error-show').innerText = "";
 })
 
-
+// Input value find 
 const valueFind = () => {
-    
     const input = document.getElementById('search-input');
     const inputValue = input.value;
     input.value = '';
     const url = `https://openlibrary.org/search.json?q=${inputValue}`;
-
     fetch(url)
         .then(val => val.json())
         .then(data => dataShow(data.docs));
 }
 
-
+// DataShow Function For Book 
 const dataShow = (data) => {
-    const containerDiv = document.getElementById('books-data');
+  const containerDiv = document.getElementById('books-data');
   containerDiv.textContent = '';
   if (data.length == 0) {
     document.getElementById('error-show').innerText = "Please search valid result";
   }
-    document.getElementById('how-result').innerText = data.length;
+  document.getElementById('how-result').innerText = data.length;
   data?.forEach(dat => {
-    
-    console.log(dat);
-
-            const imglink = `https://covers.openlibrary.org/b/id/${dat.cover_i}-M.jpg`
-        
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
             <div class="card">
-              <img src="${imglink}" class="card-img-top" alt="this image cannot upload yet">
+              <img src="${`https://covers.openlibrary.org/b/id/${dat.cover_i}-M.jpg`}" class="card-img-top" alt="this image cannot upload yet">
               <div class="card-body">
-                <p class="fs-4">First publish year : ${dat.first_publish_year ? dat.first_publish_year : 'Year not find'}</p>
-                <p class="fs-3">Publisher Company : ${dat.publisher[0] ? dat.publisher[0] : ''}</p>
-                
-                <h2>Book Name : ${dat.title ? dat.title : ''}</h2>
+                <p class="fs-4"><span class="text-danger">First publish year :</span> ${dat.first_publish_year ? dat.first_publish_year : 'not find'}</p>
+                <p class="fs-3"><span class="text-danger">Publisher Company :</span> ${dat.publisher !== undefined ? dat.publisher[0] : 'not found'}</p>
+                <h3><span class="text-danger">Writter Name :</span> ${dat.author_name !== undefined ? dat.author_name[0] : 'not found'}</h3>
+                <h2><span class="text-danger">Book Name :</span> ${dat.title ? dat.title : 'not found'}</h2>
               </div>
             </div>
             `;
     containerDiv.appendChild(div);
-    })
-    
-}
+  });
+};
